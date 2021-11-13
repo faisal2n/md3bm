4fc78a466d74ff6184a268021a14e1ddb92964dc
faisal2n <rh7al@hotmail.com>
2021-11-13 19:46:54 +0300

    Update md3bm2js.js

--- md3bm2js.js
+++ md3bm2js.js
@@ -1,4 +1,2 @@
 //Spoiler
 $(document).ready(function(){$("#flippy").click(function(){$("#flippanel").slideToggle("normal")})});
-// Youtube Responsive
-setTimeout(function(){$(".video-youtube").each(function(){$(this).replaceWith('<iframe class="video-youtube loader" src="'+$(this).data("src")+'" allowfullscreen="allowfullscreen" height="281" width="500"></iframe>')})},5e3);
